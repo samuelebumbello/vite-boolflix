f4905eb4f421f86a447ef4dd5d2712ef761c3887
samuelebumbello <samuelebumbello17@gmail.com>
2023-05-05 15:45:05 +0200

    [add] components Header,main and Axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script>
-export default {
+import axios from "axios";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
+export default {
+    name: "App",
+    components:{
+        Header,
+        Main
+    },
+    data(){
+    return {
+          store
+        }
+    },
+    methods:{
+    getApi(){
+      axios.get(store.apiUrl)
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
 }
 </script>
 
